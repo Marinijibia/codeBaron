95ab6d16c7b8afe9cd71c7e1c76b3d62c898a9dd
Umar falalu <marinijibiaa@gmail.com>
2023-06-17 17:47:25 -0700

    pushing templates for the projects

--- BaronStudybud/settings.py
+++ BaronStudybud/settings.py
@@ -45,7 +45,9 @@ ROOT_URLCONF = 'BaronStudybud.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
